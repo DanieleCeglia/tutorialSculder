889d86fe7f88793841adff40e0f6927aeb2a0012
Daniele Ceglia <daniele.ceglia@relifeit.com>
2013-12-16 10:17:00 +0100

    sistemato status bar iOS 7

--- tutorialSculder/platforms/ios/Sculder/Classes/MainViewController.m
+++ tutorialSculder/platforms/ios/Sculder/Classes/MainViewController.m
@@ -27,6 +27,12 @@
 
 #import "MainViewController.h"
 
+@interface MainViewController ()
+{
+    BOOL iOS7fixFatto;
+}
+@end
+
 @implementation MainViewController
 
 - (id)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil
@@ -37,6 +43,7 @@
         // _commandDelegate = [[MainCommandDelegate alloc] initWithViewController:self];
         // Uncomment to override the CDVCommandQueue used
         // _commandQueue = [[MainCommandQueue alloc] initWithViewController:self];
+        iOS7fixFatto = NO;
     }
     return self;
 }
@@ -61,12 +68,32 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
+#pragma mark - Colore status bar
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
 #pragma mark View lifecycle
 
 - (void)viewWillAppear:(BOOL)animated
 {
     // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
     // you can do so here.
+    
+    //Lower screen 20px on ios 7
+    if (([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) && (iOS7fixFatto == NO))
+    {
+        CGRect viewBounds = [self.webView bounds];
+        viewBounds.origin.y = 20;
+        viewBounds.size.height = viewBounds.size.height - 20;
+        self.webView.frame = viewBounds;
+        
+        self.view.backgroundColor = [UIColor blackColor];
+        
+        iOS7fixFatto = YES;
+    }
 
     [super viewWillAppear:animated];
 }
